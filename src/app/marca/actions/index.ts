30d14ff24b01bf9157aed8b98e0777ab1ee2eeca
Jeverson misael da cruz filho <jeversonmisaldacruzfilho@gmail.com>
2024-06-21 17:02:40 -0300

    FIx: errors
    on this commit i just insert

--- src/app/marca/actions/index.ts
+++ src/app/marca/actions/index.ts
@@ -1,14 +1,14 @@
-'use server'
+"use server";
 
-import {createMarca, DeleteMarca} from "@/utils/marca"
-import { revalidatePath } from "next/cache"
+import { createMarca, DeleteMarca } from "@/utils/marca";
+import { revalidatePath } from "next/cache";
 
-export default async function CreateActionMarca(nome:string) {
-  await createMarca(nome)
-  revalidatePath("/marca")
+export default async function CreateActionMarca(nome: string) {
+  await createMarca(nome);
+  revalidatePath("/marca");
 }
 
-export  async function DeleteActionMarca(id:string) {
-  await DeleteMarca(id)
-  revalidatePath("/marca")
-};
+export async function DeleteActionMarca(id: string) {
+  await DeleteMarca(id);
+  revalidatePath("/marca");
+}
